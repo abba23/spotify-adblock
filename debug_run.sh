360138c28f41bcbf018a58fd96e8dec6551148d6
Cole Leavitt <cole@unwrap.rs>
2025-10-18 12:41:21 -0700

    chore: add CEF_ROOT for debug building to `debug_run.sh`

--- debug_run.sh
+++ debug_run.sh
@@ -3,4 +3,8 @@ for pid in $(pgrep -f spotify); do
     kill -9 $pid
 done
 
-LD_PRELOAD=/home/cole/projects/spotify-adblock/target/release/libspotifyadblock.so spotify --enable-features=useozoneplatform --ozone-platform=wayland
+# If you got CEF for testing
+export CEF_ROOT="$PWD/cef_binary_137.0.19+g8a1c4ce+chromium-137.0.7151.121_linux64" && cargo check
+CEF_ROOT="$PWD/cef_binary_137.0.19+g8a1c4ce+chromium-137.0.7151.121_linux64" cargo build --release --lib
+
+LD_PRELOAD=./target/release/libspotifyadblock.so spotify --enable-features=useozoneplatform --ozone-platform=wayland
