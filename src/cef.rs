969a89c324efb1a09e3dda02aeccd3dedf284ea6
abba23 <abba23@users.noreply.github.com>
2026-06-05 19:11:48 +0200

    Minimize CEF bindings

--- src/cef.rs
+++ src/cef.rs
@@ -4,12 +4,11 @@
 pub struct _cef_request_t {
     base: [u8; 40],
     is_read_only: [u8; 8],
-    pub get_url: unsafe extern "C" fn(self_: *mut _cef_request_t) -> *mut _cef_string_utf16_t,
+    pub get_url: unsafe extern "C" fn(self_: *mut _cef_request_t) -> *mut _cef_string_utf16_t
 }
 
 #[repr(C)]
 pub struct _cef_string_utf16_t {
     pub str: *mut u16,
-    pub length: usize,
-    dtor: [u8; 8],
+    pub length: usize
 }
